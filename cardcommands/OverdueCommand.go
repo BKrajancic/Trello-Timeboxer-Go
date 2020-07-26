3afe2bf6c94d76d05f6ecf648b6d7076b4331d9f
Benjamin Krajancic <ben.k99@outlook.com.au>
2020-07-26 18:27:51 +1000

    Move cards which are overdue by twice as many days to another list

--- cardcommands/OverdueCommand.go
+++ cardcommands/OverdueCommand.go
@@ -1,15 +1,68 @@
 package cardcommands
 
-import "github.com/adlio/trello"
+import (
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/adlio/trello"
+)
 
 type OverdueCommand struct {
 	Delays map[string]float64
 }
 
 func (OverdueCommand) CheckCard(card *trello.Card) bool {
-	return card.DueComplete
+	return card.Due != nil && time.Now().After(*card.Due)
 }
 
-func (OverdueCommand) UpdateCard(card *trello.Card) error {
+func (m OverdueCommand) UpdateCard(card *trello.Card) error {
+	// Doesn't work sort by smallest to largest days
+	invertedDelays := make(map[time.Time]string)
+	maxDuesSorted := make([]time.Time, len(m.Delays))
+
+	i := 0
+
+	for match, days := range m.Delays {
+		maxDue := time.Now().Add(-time.Hour * time.Duration((24.0 * 2 * days)))
+		invertedDelays[maxDue] = match
+		maxDuesSorted[i] = maxDue
+		i++
+	}
+
+	sort.Slice(maxDuesSorted, func(i, j int) bool {
+		return maxDuesSorted[i].After(maxDuesSorted[j])
+	})
+
+	for _, maxDue := range maxDuesSorted {
+		// Filter only greater
+		if card.Due.After(maxDue) {
+			board := card.Board
+			lists, err := board.GetLists(trello.Defaults())
+
+			if err != nil {
+				return err
+			}
+
+			match := invertedDelays[maxDue]
+
+			for _, list := range lists {
+				if strings.Contains(strings.ToLower(list.Name), strings.ToLower(match)) {
+					if list.ID == card.IDList {
+						if card.Due.After(maxDue) {
+							return nil
+						}
+					} else {
+						err := card.Update(trello.Arguments{"due": ""})
+						if err != nil {
+							return err
+						}
+						return card.MoveToList(list.ID, trello.Defaults())
+					}
+				}
+			}
+		}
+	}
+
 	return nil
 }
