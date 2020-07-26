c9b2b8507bf8011d1c437843d87d7a7cd83845a6
Benjamin Krajancic <15332488+BKrajancic@users.noreply.github.com>
2020-07-26 10:43:38 +1000

    Update app.go

--- app.go
+++ app.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/BKrajancic/trellotimeboxergo/cardcommands"
-	"github.com/BKrajancic/trellotimeboxergo/config"
-	"github.com/BKrajancic/trellotimeboxergo/listcommands"
+	"trellotimeboxergo/cardcommands"
+	"trellotimeboxergo/config"
+	"trellotimeboxergo/listcommands"
 
 	"github.com/adlio/trello"
 )
